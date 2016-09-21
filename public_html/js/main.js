9f6505e9afd3a448cb747dc3ff6242e62f95d531
isokissa <pt_jr@yahoo.com>
2016-09-21 23:57:45 +0300

    Shows empty full window background
    Also jquery is upgraded.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,12 @@
+$(document).ready( function() {
+
+    var windowWidth = $(document).width();
+    var windowHeight = $(document).height();
+
+    var myLine = document.getElementById("myLine");
+    myLine.setAttribute( "x2", windowWidth );
+    myLine.setAttribute( "y2", windowHeight );
+
+});
+
 
